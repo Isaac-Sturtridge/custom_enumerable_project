a3db1dcc8b65c20123ee3364e2d2868e436cda68
izzy <eyes330@yahoo.co.uk>
2023-02-26 20:17:08 +0800

    completed enumerable methods

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,69 @@
+# enumerable
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    return unless block_given?
+
+    index = 0
+    for elem in self
+      yield elem, index
+      index += 1
+    end
+  end
+
+  def my_select
+    return unless block_given?
+
+    results = []
+    my_each { |elem| results.push(elem) if yield elem}
+    results
+  end
+
+  def my_all?
+    return unless block_given?
+
+    my_each { |elem| return false unless yield elem }
+    true
+  end
+
+  def my_any?
+    return unless block_given?
+
+    my_each { |elem| return true if yield elem }
+    false
+  end
+
+  def my_none?
+    return unless block_given?
+
+    my_each { |elem| return false if yield elem }
+    true
+  end
+
+  def my_count
+    count = 0
+    if block_given?
+      my_each { |elem| count += 1 if yield elem }
+      count
+    else
+      count = length
+    end
+  end
+
+  def my_map
+    return unless block_given?
+
+    new_arr = []
+    my_each { |elem| new_arr.push(yield elem)}
+    new_arr
+  end
+
+  def my_inject(initial=nil)
+    accum = initial || first
+
+    my_each { |elem| accum = yield(accum, elem)}
+    accum
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +72,11 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    return unless block_given?
+
+    for elem in self do
+      yield elem
+    end
+  end
 end
